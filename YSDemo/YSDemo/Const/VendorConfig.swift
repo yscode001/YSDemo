6120b64d53a422b4a865b298084f5d8e226649ba
ys <yscode@126.com>
2021-01-23 19:08:22 +0800

    三方配置

---
+++ YSDemo/YSDemo/Const/VendorConfig.swift
@@ -0,0 +1,14 @@
+//
+//  VendorConfig.swift
+//  YSDemo
+//
+//  Created by 姚帅 on 2021/1/23.
+//
+
+/// 三方库配置信息
+class VendorConfig{
+    
+    // MARK: - GME
+    static var gme_appID:String{ return "1400104761" }
+    static var gme_appKey:String{ return "viyrH8YrRyYY9A7X" }
+}
