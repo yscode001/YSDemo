329d8f23f22b0ae99103e1738ae08bb5978a52e7
ys <yscode@126.com>
2021-01-25 13:38:36 +0800

    组合动作

--- YSDemo/YSDemo/Class/SpriteKitFunc/Scene/SKScene_position.swift
+++ YSDemo/YSDemo/Class/SpriteKitFunc/Scene/SKScene_position.swift
@@ -23,7 +23,7 @@ class SKScene_position: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        pathAction()
+        speed()
     }
 }
 
@@ -94,7 +94,61 @@ extension SKScene_position{
         node.run(act)
     }
     
-    private func scale_only(){
+    // MARK: - 反向运动
+    private func reversedAction(){
+        let move = SKAction.move(by: CGVector(dx: 100, dy: 100), duration: 2)
+        let reversed = move.reversed() // 原动作的反向
         
+        // 序列动作：SKAction.sequence，按顺序执行
+        // SKAction.sequence：把一组action组合成一个action，按顺序执行，前一个执行完，后一个再执行
+        let act = SKAction.sequence([move, reversed])
+        node.run(act)
     }
 }
+
+// MARK: - 速度动作
+/*
+ 速度动作一般是配合着别的位置动作使用的，其实不但有速度动作，还有速度属性。
+ 我们可以给定一定的速度值来改变场景中节点执行动作的速度。
+ 速度动作有两个创建方式：一个是设置一个恒定的速度，加速度为0；一个是设置恒定的加速度，即速度增量。
+ */
+extension SKScene_position{
+    
+    private func speed(){
+        // 指定节点运动的速度，指定完成动作所需要的时间
+        // 速度恒定
+        let speed = SKAction.speed(to: 10, duration: 2)
+        
+        // 指定节点运动的加速度(即速度增量)，指定完成动作所需要的时间
+        // 速度越来越快
+        // let speed = SKAction.speed(by: 10, duration: 2)
+        
+        let move = SKAction.move(by: CGVector(dx: 100, dy: 500), duration: 10)
+        
+        // 组动作：SKAction.group，同时执行
+        // SKAction.group：把一组action组合成一个action，同时执行
+        let group = SKAction.group([speed, move])
+        
+        node.run(group)
+    }
+}
+
+// MARK: - 组合动作
+/*
+ 序列动作：按顺序执行
+ SKAction.sequence([move, reversed])
+ 
+ 组动作：同时执行
+ SKAction.group([speed, move])
+ 
+ 重复动作：执行重复次数、无限重复
+ SKAction.repeat(move, count: 10)
+ SKAction.repeatForever(move)
+ 
+ 组合动作注意事项:
+ 上面叙述的组动作,序列动作,重复动作是以子动作的方式包含其他动作：
+ 一个序列动作中有多个子动作，序列中的每一个动作在前一个动作结束之前开始。
+ 一个组动作中有多个子动作，动作组中的所有动作同时开始执行。
+ 一个重复动作中只有一个子动作，当重复动作中的子动作完成时，重复动作将重新开始执行。
+ 上面三种动作可以嵌套使用，这种使动作结合起来的能力可以帮你为节点添加非常复杂的动作。
+ */
